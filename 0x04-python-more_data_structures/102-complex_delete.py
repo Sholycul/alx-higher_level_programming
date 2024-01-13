8ca209b4b29f95fc6f15d22a9e77cc00bde2fd52
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-01-13 11:15:14 -0800

    Delete by key

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for x in list(a_dictionary):
+        if a_dictionary[x] == value:
+            del a_dictionary[x]
+    return a_dictionary
