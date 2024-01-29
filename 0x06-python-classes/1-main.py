a64bddd45c6ba200e9a07c2bdcb0abfe0e5caef5
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-01-28 16:25:31 -0800

    Square definition

---
+++ 0x06-python-classes/1-main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+Square = __import__('1-square').Square
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+
+guillaume@ubuntu:~/0x06$ ./1-main.py
+<class '1-square.Square'>
+{'_Square__size': 3}
+'Square' object has no attribute 'size'
+'Square' object has no attribute '__size'
