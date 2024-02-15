6b3a7b83ec5e1de1268942bda05977b6fe425d16
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-02-15 14:09:00 -0800

    Keyword args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,6 +14,19 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes an instance of the ModuleClass.
+
+        Parameters:
+        - width (int): The width of the module.
+        - height (int): The height of the module.
+        - x (int, optional): The x-coordinate of the module's position. Default is 0.
+        - y (int, optional): The y-coordinate of the module's position. Default is 0.
+        - id (Any, optional): An identifier for the module. Default is None.
+
+        Returns:
+         None
+         """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -107,16 +120,83 @@ class Rectangle(Base):
         area = self.width * self.height
         return area
 
+
     def display(self):
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+
+    def update(self, *args, **kwargs):
         """
-        print rectangle with #
+        Updates the attributes of the ModuleClass instance.
+
+        This method allows updating the module's attributes using positional arguments (*args) or keyword arguments (**kwargs).
+        When using positional arguments, the order should be (id, width, height, x, y).
+
+        Parameters:
+        - args (tuple): Positional arguments to update the module's attributes in the order (id, width, height, x, y).
+         - kwargs (dict): Keyword arguments to update specific attributes. Possible keys: 'id', 'width', 'height', 'x', 'y'.
+    
+        Returns:
+        None
         """
-        for _ in range(self.height):
-            print("#" * self.width)
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
 
