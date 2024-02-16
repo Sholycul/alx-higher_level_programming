bea0a2371e52314f4c8eb3da6537eb35b765b525
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-02-15 18:17:04 -0800

    Square inherits

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Square class file
+"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    Square class inheritance
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Square class constructot
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        String / print representation
+        """
+
+        return "[Square] ({}) {}/{} - {}".format(self.id,self.x, self.y, self.width)
