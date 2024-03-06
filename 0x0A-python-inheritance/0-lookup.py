ffca8c8787814b33a8ccbb0fdd30a5fb3add3851
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-03-06 10:56:39 -0800

    update lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
-    return ([attr for attr in dir(obj)])
+    """Returns a list
+    of available attributes and methods of an object.
+    """
+    return (dir(obj))
+
