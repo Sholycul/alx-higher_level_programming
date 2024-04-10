4953815e24ce94345148254fee7b0fdb20ac48e9
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-04-10 08:13:05 -0700

    concatenation

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const [, , arg1, arg2] = process.argv;
+
+/* if (arg1 === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg1);
+} */
+console.log(arg1, 'is ', arg2);
