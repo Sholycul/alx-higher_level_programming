a8229224189b510a5a55bd677ee5aacbcedc4c06
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-04-10 08:29:09 -0700

    to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const [,,arg1] = process.argv;
+
+const num = parseInt(arg1);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
