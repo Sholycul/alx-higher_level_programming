c203f857935662bafc7a494c04693c34da6c2819
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-04-10 08:43:20 -0700

    Foor loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = languages.length;
+
+for (let i = 0; i < len; i++) {
+  console.log(languages[i]);
+}
