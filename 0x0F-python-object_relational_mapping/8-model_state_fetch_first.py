23b0b9844b7bd2f9bdf97ecb918e8c4e101f1755
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-04-18 21:03:48 -0700

    Model filters

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Script to print the first State object from the database hbtn_0e_6_usa.
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Script to list all State objects containing the letter 'a'
