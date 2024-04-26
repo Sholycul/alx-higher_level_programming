e0ece5deae8027bc3a7edea2e818f35085fb0c38
Sholycul <micaheloluwasholaiji2020@gmail.com>
2024-04-26 10:18:15 -0700

    Added my github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""My profile id"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/users/{argv[1]}"
+
+    headers = {"accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {argv[2]}"}
+
+
+    response = requests.get(url, headers=headers)
+    profile = response.json()
+
+    idx = profile["id"]
+    print(idx)
