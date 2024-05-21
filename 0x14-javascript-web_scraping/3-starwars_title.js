80efb9727f99cdaf2e5e5e4a3b15a3dbeeb8f14c
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-05-21 15:15:17 -0700

    Star wars data

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+//star wars movie data
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
+});
