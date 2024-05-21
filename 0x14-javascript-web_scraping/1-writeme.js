f5773c350ed3496d3c7ef64f45df54155e98bb3e
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-05-21 14:51:42 -0700

    Write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,10 @@
 // writing to a file
 
 const fs = require('fs');
-const content = process.argv[3];
 
-fs.writeFile(process.argv[2], content, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(content);
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
